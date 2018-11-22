5d9bf57500e3145874177a65b4ed6a8a3af7bdfa
gabriel.silva <gabriel.silva@fiocruz.br>
2018-11-21 23:48:42 -0200

    Added Retrieve and update operations

--- src/resources.py
+++ src/resources.py
@@ -1,5 +1,6 @@
 from app import Session
 from models.ProductModel import Product, ProductSchema
+from falcon import falcon
 
 
 class ProductView:
@@ -8,16 +9,25 @@ class ProductView:
     def __init__(self):
         self.session = Session
 
-    def on_get(self, req, resp, id):
-        req.context['result'] = self.session.query(Product).all()
+    def on_get(self, req, resp, product_id=None):
+        """ List or Retrieve Operation """
+        if not product_id:
+            req.context['result'] = self.session.query(Product).all()
+
+        #Retrieve Operation.
+        result = self.session.query(Product).filter_by(id=product_id).first()
+        if result:
+            req.context['result'] = result
+        resp.status = falcon.HTTP_404
 
     def on_post():
+        """ Create Operation """
         pass
 
     def on_put(self, req, resp, **kwargs):
         data = req.context['json']
         if isinstance(data, Product):
             self.session.add(data)
-            response = self.session.commit()
-            print(data.id)
-        pass
+            self.session.commit()
+            
+        
