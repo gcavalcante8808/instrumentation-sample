ca4049b234d84ea62d15cdce311984a71f3d997c
gabriel.silva <gabriel.silva@fiocruz.br>
2018-12-10 07:10:06 -0200

    Removed Counter.

--- src/client.py
+++ src/client.py
@@ -5,11 +5,7 @@ import pandas as pd
 import json
 import random
 
-from collections import Counter
-
-
 SERVER_PORT = os.getenv('SERVER_PORT', 5000)
-
 url = 'http://localhost:{}/products'.format(SERVER_PORT)
 
 enterprises = (1,2,3,4,5)
@@ -26,7 +22,6 @@ async def create_product():
     response = requests.post(url, data=json.dumps(data), headers=headers)
 
     assert response.status_code, 201
-    print(data)
     execution_time.append(response.elapsed.total_seconds())
     execution_results.append(response.text)
 
@@ -38,7 +33,6 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop))
     print("#### Create Product Routine ###")
-    print(Counter(execution_results))
     s = pd.Series(execution_time)
     print(s.describe())
 
